faba2c8b243c7a8ab4e7c0528f2fc3bc948aaf14
sakura-fly <sourcesakura@sina.com>
2018-04-24 22:59:19 +0800

    新建controller

---
+++ src/main/java/com/libmss/controller/AdminController.java
@@ -0,0 +1,4 @@
+package com.libmss.controller;
+
+public class AdminController {
+}
---
+++ src/main/java/com/libmss/controller/PeriodicalController.java
@@ -0,0 +1,9 @@
+package com.libmss.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/periodical")
+public class PeriodicalController {
+}
