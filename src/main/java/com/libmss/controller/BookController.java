c05a8c99d3e45765f68b071ce6783ff44ecfe82a
sakura-fly <sourcesakura@sina.com>
2018-04-26 19:59:42 +0800

    book

--- src/main/java/com/libmss/controller/BookController.java
+++ src/main/java/com/libmss/controller/BookController.java
@@ -1,4 +1,28 @@
 package com.libmss.controller;
 
+
+import com.libmss.model.Book;
+import com.libmss.model.PageModel;
+import com.libmss.model.ResponseModel;
+import com.libmss.service.impl.BookServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RequestMapping("/book")
 public class BookController {
+
+
+    @Autowired
+    BookServiceImpl bookService;
+
+    @RequestMapping(value = "/list", method = RequestMethod.POST)
+    @ResponseBody
+    public ResponseModel<Book> list(Book book, PageModel pageModel){
+        return bookService.list(pageModel,book);
+    }
+
 }
