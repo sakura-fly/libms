c7b0e9074e5b2ec4218f402d7c6f60e488a7b1ed
sakura-fly <sourcesakura@sina.com>
2018-05-07 17:33:00 +0800

    添加MySQL文件，修改登录

--- src/main/java/com/libmss/dao/impl/BaseDaoImpl.java
+++ src/main/java/com/libmss/dao/impl/BaseDaoImpl.java
@@ -10,22 +10,25 @@ import org.hibernate.Transaction;
 
 import java.io.Serializable;
 import java.lang.reflect.Field;
+import java.util.ArrayList;
 import java.util.List;
 
 public abstract class BaseDaoImpl<T> implements BaseDao<T> {
 
     private Session session;
     protected StringBuffer selectSql = new StringBuffer();
-
+    protected SessionFactory sessionFactory;
 
 
     public BaseDaoImpl(SessionFactory sessionFactory) {
-        session = sessionFactory.openSession();
+        // session = sessionFactory.openSession();
+        this.sessionFactory = sessionFactory;
     }
 
     @Override
     public int add(T t) {
         int stat = -1;
+        session = sessionFactory.openSession();
         Transaction tt = session.beginTransaction();
         try {
             // session.
@@ -33,6 +36,7 @@ public abstract class BaseDaoImpl<T> implements BaseDao<T> {
             Serializable s = session.save(t);
 
             tt.commit();
+            session.close();
             // session.close()
             stat = 1;
         } catch (Exception e) {
@@ -50,59 +54,66 @@ public abstract class BaseDaoImpl<T> implements BaseDao<T> {
     public List<T> list(PageModel pageModel, T t) {
 
 
-        // Class<User> uc = User.class;
+        List<T> res = new ArrayList<>();
         Class<?> c = t.getClass();
         Field[] uf = c.getDeclaredFields();
         String  selectSqlOld = selectSql.toString();
+        session = sessionFactory.openSession();
         Transaction tx = session.beginTransaction();
-        for (int i = 0; i < uf.length; i++) {
-            Field f = uf[i];
-            f.setAccessible(true);
-            String name = f.getName();
-            try {
-                Object val = f.get(t);
-                String type = f.getType().toString();
-                if (type.endsWith( "int" ) || type.endsWith( "Integer" )){
-                    if ((int)val != -2233){
-                        selectSql.append(" and " + name + " like :" + name);
+        try {
+            for (int i = 0; i < uf.length; i++) {
+                Field f = uf[i];
+                f.setAccessible(true);
+                String name = f.getName();
+                try {
+                    Object val = f.get(t);
+                    String type = f.getType().toString();
+                    if (type.endsWith( "int" ) || type.endsWith( "Integer" )){
+                        if ((int)val != -2233){
+                            selectSql.append(" and " + name + " like :" + name);
+                        }
+                    } else {
+                        if (val != null && !val.toString().isEmpty()){
+                            selectSql.append(" and " + name + " like :" + name);
+                        }
                     }
-                } else {
-                    if (val != null && !val.toString().isEmpty()){
-                        selectSql.append(" and " + name + " like :" + name);
-                    }
+                } catch (IllegalAccessException e) {
+                    e.printStackTrace();
                 }
-            } catch (IllegalAccessException e) {
-                e.printStackTrace();
-            }
 
-        }
-        SQLQuery sql = session.createSQLQuery(selectSql.toString());
-        selectSql = new StringBuffer(selectSqlOld);
-        sql.addEntity(t.getClass());
-        for (int i = 0; i < uf.length; i++) {
-            Field f = uf[i];
-            f.setAccessible(true);
-            String name = f.getName();
-            try {
-                Object val = f.get(t);
-                String type = f.getType().toString();
-                if (type.endsWith( "int" ) || type.endsWith( "Integer" )){
-                    if ((int)val != -2233){
-                        // selectSql.append(" and " + name + " = :" + name);
-                        sql.setParameter(name,"%" + val + "%");
+            }
+            SQLQuery sql = session.createSQLQuery(selectSql.toString());
+            selectSql = new StringBuffer(selectSqlOld);
+            sql.addEntity(t.getClass());
+            for (int i = 0; i < uf.length; i++) {
+                Field f = uf[i];
+                f.setAccessible(true);
+                String name = f.getName();
+                try {
+                    Object val = f.get(t);
+                    String type = f.getType().toString();
+                    if (type.endsWith( "int" ) || type.endsWith( "Integer" )){
+                        if ((int)val != -2233){
+                            // selectSql.append(" and " + name + " = :" + name);
+                            sql.setParameter(name,"%" + val + "%");
+                        }
+                    } else {
+                        if (val != null && !val.toString().isEmpty()){
+                            sql.setParameter(name,"%" + val + "%");
+                        }
                     }
-                } else {
-                    if (val != null && !val.toString().isEmpty()){
-                        sql.setParameter(name,"%" + val + "%");
-                    }
+                } catch (IllegalAccessException e) {
+                    e.printStackTrace();
                 }
-            } catch (IllegalAccessException e) {
-                e.printStackTrace();
-            }
 
+            }
+            res = sql.list();
+        } catch (Exception e){
+            e.printStackTrace();
         }
         tx.commit();
-        return sql.list();
+        session.close();
+        return res;
     }
 
     @Override
